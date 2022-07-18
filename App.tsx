3ef0a84c46aa358a79b9fc5f9c36fbee16593846
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-07-18 17:56:38 -0300

    feat: Pagina de SignIn

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NativeBaseProvider, StatusBar} from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { THEME } from './src/styles/theme';
+import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+     {fontsLoaded ? <SignIn /> : <Loading/>} 
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/input.tsx
@@ -0,0 +1,7 @@
+import {Input as NativeBaseInput} from 'native-base';
+
+export function Input() {
+  return(
+   <NativeBaseInput bg="gray.700" h={14} size="md" fontFamily="body" color="white" placeholderTextColor="gray.300"/>
+  );
+}
